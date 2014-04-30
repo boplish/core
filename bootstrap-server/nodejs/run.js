dad569e8462dc5295be817bc2d1458801799cff3
Christian Vogt <mail@christianvogt.de>
2014-04-30 11:35:40 +0200

    Moved bootstrap server to its own repo

--- bootstrap-server/nodejs/run.js
+++
@@ -1,6 +0,0 @@
-var BootstrapServer = require('./bootstrapserver.js');
-var hostname = process.argv[2];
-var port = process.argv[3];
-var staticPath = process.argv[4];
-var server = new BootstrapServer(hostname, port, staticPath);
-server.listen();
--- bootstrap-server/python/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(dirname $0)
-
-. $BASEDIR/../bin/activate
-python $BASEDIR/run.py
